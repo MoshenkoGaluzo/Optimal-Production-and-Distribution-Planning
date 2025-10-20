9255370dc5ff01b4fba1ce42c819be629da79e40
MoshenkoGaluzo <mosenko-galuzo@proton.me>
2025-10-20 14:31:41 +0100

    little update

--- src/solver.py
+++ src/solver.py
@@ -1,24 +1,9 @@
 from pulp import *
 import data_preperation as dp
  
-m=4
-n=2
-indexI = range(1,m+1)
-indexJ = range(1,n+1)
-indexK = range(1, 3)
-
-A = [ [6, 4], [1, 2], [-1, 1], [0, 1] ]
-B = [24, 6, 1, 2]
-C = [5, 4]
-A = makeDict([indexI, indexJ], A)
-B = makeDict([indexI], B)
-C = makeDict([indexJ], C)
-
-
 model_Drinks = LpProblem("Drinks", LpMaximize)
 
 X = []
-#print(X[0][0][0])
 
 Drinks = ["Iced Tea", "Sparkling Water", "Natural Juice","Flavored Mineral Water"]
 Cities = ["Lisbon", "Madrid", "Barcelona", "Paris", "Marseille", "Porto"]
@@ -31,8 +16,6 @@ for i in range(0,4):
         for k in range(0,3):
             X[i][j].append(LpVariable(f"{Drinks[i]} in {Cities[j]} from {Factories[k]}", 0, None))
             #X[i][j].append(f"{Drink[i]} in {City[j]} from {Factory[k]}")
-
-#print(X[0][0][0])
 
 #Profit function
 #price/l - cost/l - transport/l
