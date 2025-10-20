7fa855c333a7a52efce1c1b26245e29156234ffc
MoshenkoGaluzo <mosenko-galuzo@proton.me>
2025-10-20 11:10:25 +0100

    Variables addes

---
+++ src/solver.py
@@ -0,0 +1,31 @@
+from pulp import *
+import data_preperation as dp
+ 
+m=4
+n=2
+indexI = range(1,m+1)
+indexJ = range(1,n+1)
+indexK = range(1, 3)
+
+A = [ [6, 4], [1, 2], [-1, 1], [0, 1] ]
+B = [24, 6, 1, 2]
+C = [5, 4]
+A = makeDict([indexI, indexJ], A)
+B = makeDict([indexI], B)
+C = makeDict([indexJ], C)
+
+X = []
+#print(X[0][0][0])
+
+Drink = ["Iced Tea", "Sparkling Water", "Natural Juice","Flavored Mineral Water"]
+City = ["Lisbon", "Madrid", "Barcelona", "Paris", "Marseille", "Porto"]
+Factory = ["Portugal", "Spain", "France"]
+
+for i in range(0,4):
+    X.append([])
+    for j in range(0,6):
+        X[i].append([])
+        for k in range(0,3):
+            X[i][j].append(LpVariable(f"{Drink[i]} in {City[j]} from {Factory[k]}"))
+
+print(X[0][0][0])
